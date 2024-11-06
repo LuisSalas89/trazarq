e37a71c43e1ad76afc97f802bb72ba943ea22ba9
Luis <lar_8912@hotmail.com>
2024-11-05 21:55:51 -0600

    resolve pdf

--- src/components/Dictamen.jsx
+++ src/components/Dictamen.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../styles/dictamen.css";
 
 function Dictamen() {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+  useEffect(() => {
+    if (isMobile) {
+      window.open(`${process.env.PUBLIC_URL}/Dictamen.pdf`, '_blank');
+    }
+  }, [isMobile]);
+
   return (
     <div className="pdfContainer">
       {isMobile ? (
-        <button
-          onClick={() => window.open(`${process.env.PUBLIC_URL}/Dictamen.pdf`, '_blank')}
-          className="pdfButton"
-        >
-          Abrir Dictamen
-        </button>
+        <p>El dictamen debería abrirse automáticamente. Si no se abre, haz clic <a href={`${process.env.PUBLIC_URL}/Dictamen.pdf`} target="_blank" rel="noopener noreferrer">aquí</a>.</p>
       ) : (
         <iframe
           src={`${process.env.PUBLIC_URL}/Dictamen.pdf`}
@@ -27,4 +28,3 @@ function Dictamen() {
 }
 
 export default Dictamen;
-
