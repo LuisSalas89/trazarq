5663ba18b02adc60b469bf4116975fd3f26c002b
Luis <lar_8912@hotmail.com>
2024-11-05 21:50:37 -0600

    resolve pdf

--- src/components/Dictamen.jsx
+++ src/components/Dictamen.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import "../styles/dictamen.css"
+import React from 'react';
+import "../styles/dictamen.css";
 
 function Dictamen() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   return (
     <div className="pdfContainer">
-    <iframe src={`${process.env.PUBLIC_URL}/Dictamen.pdf`} type="application/pdf"
-     width="100%"
-     height="100%"
-     title="PDF Viewer"
-    ></iframe>
-</div>
-  )
+      {isMobile ? (
+        <a href={`${process.env.PUBLIC_URL}/Dictamen.pdf`} download="Dictamen.pdf">
+          Descargar Dictamen
+        </a>
+      ) : (
+        <iframe
+          src={`${process.env.PUBLIC_URL}/Dictamen.pdf`}
+          type="application/pdf"
+          width="100%"
+          height="100%"
+          title="PDF Viewer"
+        ></iframe>
+      )}
+    </div>
+  );
 }
 
-export default Dictamen
+export default Dictamen;
